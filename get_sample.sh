d8a2b4c232155e3f9af6119f9f3ffceaf30dbafa
sheridan <142021666+sheridan-brendan@users.noreply.github.com>
2024-09-03 17:23:56 -0400

    sanity commit before concurrency refactor

--- get_sample.sh
+++ get_sample.sh
@@ -3,4 +3,4 @@
 outfile="${1%.*}"
 
 ffmpeg -i $1 -ss 300 -t 00:00:30 -vcodec copy $outfile-samp.mp4
-ffmpeg -ss 300 -i $1 -t 00:00:30 -vcodec copy $outfile-insamp.mp4
+#ffmpeg -ss 300 -i $1 -t 00:00:30 -vcodec copy $outfile-insamp.mp4
